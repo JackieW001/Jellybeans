ab54bdb6d442b52adb276c1b6ec965144bca4638
JackieW00 <jwoo@stuy.edu>
2017-03-09 05:27:40 -0500

    add QuickSortTester

---
+++ QuickSortTester.java
@@ -0,0 +1,26 @@
+public static QuickSortTester{
+
+    public static void main (String[] args){
+	QuickSort qs = new QuickSort();
+
+
+	int [] arr1 = {(int)(Math.random()*100)};
+	qs.qsort(arr1);
+	/*
+	int[] arr10 = new int[10];
+	for (int i = 0; i < arr10.length; i++){
+	    arr10[i] = (int)(Math.random()*100);
+	}
+	int[] arr100 = new int[100];
+	for (int i = 0; i < arr100.length; i++){
+	    arr100[i] = (int)(Math.random()*100);
+	}
+	
+	int[] arr1000 = new int[1000];
+	for (int i = 0; i < arr1000.length; i++){
+	    arr1000[i] = (int)(Math.random()*100);
+	}
+	*/
+	
+    }
+}
